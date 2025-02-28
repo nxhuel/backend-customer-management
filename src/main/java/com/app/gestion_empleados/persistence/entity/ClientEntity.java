24a1e176d4e60fa12d9485984e3151247d135162
nxhuel <nahueltisera03@gmail.com>
2025-02-27 21:44:24 -0300

    prueba

--- src/main/java/com/app/gestion_empleados/persistence/entity/ClientEntity.java
+++ src/main/java/com/app/gestion_empleados/persistence/entity/ClientEntity.java
@@ -1,15 +1,13 @@
 package com.app.gestion_empleados.persistence.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
 
-import java.util.UUID;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
 @Table
 @Entity(name = "client")
 public class ClientEntity
@@ -24,4 +22,56 @@ public class ClientEntity
     private String lastname;
     @Column(name = "email")
     private String email;
+
+    public ClientEntity()
+    {
+    }
+
+    public ClientEntity(long id, String name, String lastname, String email)
+    {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getLastname()
+    {
+        return lastname;
+    }
+
+    public void setLastname(String lastname)
+    {
+        this.lastname = lastname;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
 }
